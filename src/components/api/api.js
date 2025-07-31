6b8a6ddffe2c75f2b38534aa09b0b055a10a767b
Anuj Maurya <anuj24122001@gmail.com>
2025-07-31 22:28:32 +0530

    correcting fetching for deployment

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,12 +1,24 @@
 // src/api/api.js
 
 export async function fetchBatches() {
+  const API_BASE =
+  import.meta.env.MODE === 'development'
+    ? '/api'
+    : 'https://recruitinator.dpschool.io/api'
+  const resp = await fetch(`${API_BASE}/raw/batches`)  
   // Replace with your real API in production
-  const resp = await fetch("/api/raw/batches");
+  // const resp = await fetch("/api/raw/batches");
   return resp.json();
 }
 
 export async function fetchTracks() {
-  const resp = await fetch("/api/raw/tracks");
+  const API_BASE =
+  import.meta.env.MODE === 'development'
+    ? '/api'
+    : 'https://recruitinator.dpschool.io/api'
+
+// Then use:
+const resp = await fetch(`${API_BASE}/raw/tracks`)
+  // const resp = await fetch("/api/raw/tracks");
   return resp.json();
 }
