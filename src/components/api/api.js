fa43a77fc36ded5bfbf2f9a411290d1752b2e6c4
Anuj Maurya <anuj24122001@gmail.com>
2025-07-31 22:48:58 +0530

    with env

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,21 +1,16 @@
 // src/api/api.js
-
+const API_BASE = import.meta.env.VITE_API_BASE;
 export async function fetchBatches() {
-  const API_BASE =
-  import.meta.env.MODE === 'development'
-    ? '/api'
-    : 'https://recruitinator.dpschool.io/api'
-  const resp = await fetch(`${API_BASE}/raw/batches`)  
+   
+
+  const resp = await fetch(`${API_BASE}/raw/batches`)
   // Replace with your real API in production
   // const resp = await fetch("/api/raw/batches");
   return resp.json();
 }
 
 export async function fetchTracks() {
-  const API_BASE =
-  import.meta.env.MODE === 'development'
-    ? '/api'
-    : 'https://recruitinator.dpschool.io/api'
+  
 
 // Then use:
 const resp = await fetch(`${API_BASE}/raw/tracks`)
